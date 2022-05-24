2e4e66d6eb72d1b4a6b15a3137dbb42053e799b5
Piotr Jakubowski <jakubowski656@gmail.com>
2022-05-24 21:06:47 +0200

    feat(homepage): table grid

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="list">
-    <n-h1>
-      <n-text style="text-align: center"> Tasks </n-text>
-    </n-h1>
+    <n-grid cols="12" item-responsive x-gap="24">
+      <n-grid-item span="12" style="margin-bottom: 24px">
+        <n-h1>
+          <n-text style="text-align: center"> Tasks</n-text>
+        </n-h1>
+      </n-grid-item>
 
-    <Table />
+      <n-grid-item offset="0 640:2" span="12 640:8">
+        <Table/>
+      </n-grid-item>
+    </n-grid>
   </div>
 </template>
+
+<script>
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+  setup() {
+    const tasks = ref([]);
+
+    return {
+      tasks,
+    };
+  },
+});
+</script>
